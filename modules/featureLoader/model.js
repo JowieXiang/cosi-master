e7a7d1232d608a3b0406ed598969262e53c5b448
YuxiangZhang1993 <zyx1993@126.com>
2019-09-24 17:20:49 +0200

    update - add stadtteil(sports/greenspace) layer to compareDistricts. refactor featureLoader. todo: process sports/greenspace data in featureLoader

--- modules/featureLoader/model.js
+++ modules/featureLoader/model.js
@@ -1,50 +1,140 @@
-
 const FeatureLoaderModel = Backbone.Model.extend({
     defaults: {
+        layerGroups: {
+            "stadtteil": [
+                "1694"
+            ],
+            "stadtteil-layers": [
+                "2763",
+                "2769",
+                "2768",
+                "2771"
+            ],
+            "statgebiet-layers": [
+                "bev_gesamt",
+                "bev_maennlich",
+                "bev_weiblich"
+            ],
+            "Sportstätten-layer": [
+                "12868"
+            ],
+            "Grünflächen-layer": [
+                "1534"
+            ],
+            "Kindertagesstätten-layer": [
+                "753"
+            ]
+        },
         featureCollections: []
     },
     initialize: function () {
-        var channel = Radio.channel("FeatureLoader");
+        this.channel = Radio.channel("FeatureLoader");
 
-        channel.reply({
+        this.channel.reply({
             "getFeaturesByLayerId": this.getFeaturesByLayerId
         }, this);
 
         this.listenTo(Radio.channel("Layer"), {
             "featuresLoaded": function (layerId, features) {
-                var currentLayerIds = [];
-                // selector = Radio.request("SelectDistrict", "getSelector");
-
-                _.each(features, feature => {
-                    if (_.contains(Object.keys(feature.getProperties()), "stadtteil")) {
-                        let stadtteil = feature.getProperties().stadtteil;
-
-                        stadtteil = stadtteil.replace(/_/g, "");
-                        stadtteil = stadtteil.replace(/-/g, "");
-                        stadtteil = stadtteil.replace(/\s+/g, "");
-                        feature.set("stadtteil", stadtteil);
-                    }
-                    // move "stat_gebiet" value to "statgebiet" for gebiet features
-                    if (_.contains(Object.keys(feature.getProperties()), "stat_gebiet")) {
-                        feature.set("statgebiet", feature.getProperties().stat_gebiet);
-                    }
-                }, this);
-                if (this.get("featureCollections").length > 0) {
-                    currentLayerIds = this.get("featureCollections").map(collection => collection.layerId);
-                }
-                if (!_.contains(currentLayerIds, layerId)) {
-                    channel.trigger("addFeatureCollection", layerId, features);
-                    this.pushFeatureCollection(layerId, features);
-                }
+                console.log("layer:", layerId);
+                this.processData(layerId, features);
             }
         });
     },
+    processData: function (layerId, features) {
+        const currentLayerIds = this.get("featureCollections").length > 0 ? this.get("featureCollections").map(collection => collection.layerId) : [],
+            layerCategory = _.allKeys(this.get("layerGroups")).filter(groupName => {
+                const layerGroup = this.get("layerGroups")[groupName];
+
+                return _.contains(layerGroup, layerId);
+            });
+        // console.log("layerGroup: ", layerCategory, layerId);
+
+        if (!_.contains(currentLayerIds, layerId) && layerCategory.length > 0) {
+            switch (layerCategory[0]) {
+                case "stadtteil-layers": {
+                    this.processStatteilData(features);
+                    break;
+                }
+                case "stadtteil": {
+                    this.processStatteilData(features);
+                    break;
+                }
+                case "statgebiet-layers": {
+                    this.processStatgebietData(features);
+                    break;
+                }
+                case "Sportstätten-layer": {
+                    this.processSportsData(features);
+                    break;
+                }
+                case "Grünflächen-layer": {
+                    this.processGreenData(features);
+                    break;
+                }
+                case "Kindertagesstätten-layer": {
+                    this.processKinderData(features);
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+            this.channel.trigger("addFeatureCollection", layerId, features);
+            this.pushFeatureCollection(layerId, features);
+        }
+    },
+    processStatteilData: function (features) {
+        _.each(features, feature => {
+            if (_.contains(Object.keys(feature.getProperties()), "stadtteil")) {
+                let stadtteil = feature.getProperties().stadtteil.replace(/_/g, "");
+
+                stadtteil = stadtteil.replace(/-/g, "");
+                stadtteil = stadtteil.replace(/\s+/g, "");
+                feature.set("stadtteil", stadtteil);
+            }
+        }, this);
+    },
+    processStatgebietData: function (features) {
+        _.each(features, feature => {
+            // move "stat_gebiet" value to "statgebiet" for gebiet features
+            if (_.contains(Object.keys(feature.getProperties()), "stat_gebiet")) {
+                feature.set("statgebiet", feature.getProperties().stat_gebiet);
+            }
+        }, this);
+    },
+    processSportsData: function (features) {
+        const featuresByStatteil = _.groupBy(features, function (feature) {
+            return feature.getProperties().stadtteil;
+        }),
+            districtLayer = this.get("featureCollections").filter(collection => collection.layerId === this.get("layerGroups").stadtteil[0])[0];
+        // console.log("featuresByStatteil: ", featuresByStatteil);
+
+        // console.log("districtLayer: ", districtLayer);
+        _.each(districtLayer.collection, district => {
+            district.set("sports", []);
+            console.log("district.getProperties: ", district.getProperties().sports);
+
+        });
+    },
+    processGreenData: function (features) {
+        _.each(features, feature => {
+
+        }, this);
+    },
+    processKinderData: function (features) {
+        _.each(features, feature => {
+
+        }, this);
+    },
+
     sendAddedLayer: function (layerId, features) {
         return {
             "layerId": layerId,
             "collection": features
         };
     },
+
     // push feature collection to this model
     pushFeatureCollection: function (layerId, features) {
         var featureCollections = this.get("featureCollections");
@@ -53,8 +143,10 @@ const FeatureLoaderModel = Backbone.Model.extend({
             "layerId": layerId,
             "collection": features
         });
+        // console.log("featurepushed: ", features[0].getProperties());
         this.set("featureCollections", featureCollections);
     },
+
     getFeaturesByLayerId: function (layerId) {
         const selectedCollection = this.get("featureCollections").filter(collection => collection.layerId === layerId);
         let features = [];
