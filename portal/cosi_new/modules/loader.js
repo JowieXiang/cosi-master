70aca34145929d1b091ef4ae128f8671c65c3300
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2019-10-16 12:34:35 +0200

    update - timeslider as custom module

--- portal/cosi_new/modules/loader.js
+++ portal/cosi_new/modules/loader.js
@@ -3,9 +3,15 @@ import FeaturesLoader from "./featuresLoader/model";
 
 import SaveSelectionCosiView from "./saveSelection/view";
 import SaveSelectionCosi from "./saveSelection/model";
+import TimeSliderView from "./timeSlider/view";
+import TimeSlider from "./timeSlider/model";
 
 const tools = {
     SaveSelectionCosi: new SaveSelectionCosi({
+        parentId: "tools",
+        type: "tool"
+    }),
+    TimeSlider: new TimeSlider({
         parentId: "tools",
         type: "tool"
     })
@@ -20,6 +26,7 @@ function initializeCosi () {
     Radio.trigger("ModelList", "addModelsAndUpdate", Object.values(tools));
 
     new SaveSelectionCosiView({model: tools.SaveSelectionCosi});
+    new TimeSliderView({model: tools.TimeSlider});
 }
 
 export default initializeCosi;
---
+++ portal/cosi_new/modules/timeSlider/view.js
@@ -0,0 +1,105 @@
+import Template from "text-loader!./template.html";
+import SnippetDropdownView from "../../../../modules/snippets/dropdown/view";
+import SnippetSliderView from "../../../../modules/snippets/slider/view";
+
+const TimeSeriesView = Backbone.View.extend({
+    events: {
+        "click button.btn-slider": "buttonClickCallback",
+        "click button.close": function () {
+            this.model.setIsActive(false);
+        }
+    },
+    initialize: function () {
+        this.listenTo(this.model, {
+            "render": this.render,
+            "renderDropDownView": this.renderDropDownView,
+            "renderSliderView": this.renderSliderView,
+            "renderGraph": this.renderGraph,
+            "stopRunning": this.setButtonToPlay,
+            "remove": this.remove
+        });
+    },
+    id: "time-series",
+    template: _.template(Template),
+    render: function () {
+        var attr = this.model.toJSON();
+
+        this.$el.html(this.template(attr));
+        Radio.trigger("Sidebar", "append", this.el);
+        Radio.trigger("Sidebar", "toggle", true, "40%");
+        this.delegateEvents();
+
+        return this;
+    },
+
+    renderDropDownView: function (dropdownModel) {
+        const dropdownView = new SnippetDropdownView({model: dropdownModel});
+
+        this.$el.find(".dropdown").append(dropdownView.render().el);
+    },
+
+    renderSliderView: function (sliderModel) {
+        const sliderView = new SnippetSliderView({model: sliderModel});
+
+        this.$el.find(".slider-container").remove();
+        this.$el.find(".time-series-slider").prepend(sliderView.render().$el);
+    },
+
+    remove: function () {
+        this.$el.remove();
+        Radio.trigger("Sidebar", "toggle", false);
+    },
+
+    buttonClickCallback: function () {
+        if (this.model.get("isRunning")) {
+            this.setButtonToPlay();
+            this.model.setIsRunning(false);
+        }
+        else {
+            this.setButtonToPause();
+            this.model.setIsRunning(true);
+            this.model.runningTimeSeries();
+        }
+    },
+
+    setButtonToPlay: function () {
+        this.$el.find("button > .glyphicon").addClass("glyphicon-play");
+        this.$el.find("button > .glyphicon").removeClass("glyphicon-pause");
+    },
+
+    setButtonToPause: function () {
+        this.$el.find("button > .glyphicon").addClass("glyphicon-pause");
+        this.$el.find("button > .glyphicon").removeClass("glyphicon-play");
+    },
+
+    renderGraph: function (graphData, value, getMaxYAxisValue) {
+        this.$el.find(".time-series-graph").empty();
+
+        Radio.trigger("Graph", "createGraph", {
+            graphType: "BarGraph",
+            selector: ".time-series-graph",
+            scaleTypeX: "ordinal",
+            scaleTypeY: "linear",
+            data: graphData,
+            attrToShowArray: ["jahr_" + value],
+            xAttr: "stat_gebiet",
+            xAxisLabel: {
+                "rotate": 45
+            },
+            yAxisLabel: {
+                "maxValue": getMaxYAxisValue
+            },
+            margin: {
+                left: 60,
+                top: 20,
+                right: 20,
+                bottom: 40
+            },
+            width: document.getElementsByClassName("sidebar")[0].offsetWidth - 20,
+            height: document.getElementsByClassName("sidebar")[0].offsetHeight - (document.getElementsByClassName("sidebar")[0].offsetHeight * 0.75),
+            svgClass: "time-series-graph-svg"
+        });
+    }
+});
+
+export default TimeSeriesView;
