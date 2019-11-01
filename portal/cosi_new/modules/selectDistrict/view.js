d4f78a7233948b9fcb0d22cd043fbf311b43aac8
PinkyUndDerBrain <sebastian.duden@gv.hamburg.de>
2019-11-01 11:58:55 +0100

    fix - greying out districts

--- portal/cosi_new/modules/selectDistrict/view.js
+++ portal/cosi_new/modules/selectDistrict/view.js
@@ -1,4 +1,3 @@
-import SelectDistrictModel from "./model";
 import Template from "text-loader!./template.html";
 import SnippetDropdownView from "../../../../modules/snippets/dropdown/view";
 
@@ -14,6 +13,14 @@ const SelectDistrictView = Backbone.View.extend({
 
         this.listenTo(this.model, {
             "change:isActive": this.render
+        });
+
+        // hack as long as no stadtteile are available
+        this.listenTo(this.scopeDropdownView.model, {
+            "change:isOpen": function () {
+                this.$el.find(".dropdown-menu.inner li:nth-child(2)").addClass("disabled");
+                this.$el.find(".dropdown-menu.inner li:nth-child(2)").prop("title", "Stadtteile zurzeit noch nicht verfügbar");
+            }
         });
 
         if (this.model.get("isActive") === true) {
@@ -35,9 +42,6 @@ const SelectDistrictView = Backbone.View.extend({
             this.$el.html(this.template(attr));
 
             this.$el.find(".dropdown").append(this.scopeDropdownView.render().el);
-            // hack as long as no stadtteile are available
-            this.$el.find(".dropdown-menu.inner li:nth-child(2)").addClass("disabled");
-            this.$el.find(".dropdown-menu.inner li:nth-child(2)").prop("title", "Stadtteile zurzeit noch nicht verfügbar");
         }
         return this;
     },
